1055c3997b2a5973d3a4519bd38546cc41da1f50
= <lucasregoaraujo@gmail.com>
2023-02-26 18:49:08 -0300

    Removendo o lint e Transformando o componente de stateless parastateful

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -2,15 +2,57 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(PerguntaApp());
   
+class PerguntaAppState extends State<PerguntaApp>{
 
+  int perguntaSelecionada = 0;
 
-class PerguntaApp extends StatelessWidget{
+  void responder(){
+    setState(() {
+      perguntaSelecionada++;
+    });
+     print('Pergunta respondida!');
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Text('\nOlá Flutter!'),
+
+    final perguntas = [
+    'Qual é a sua cor favorita?',
+    'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+             Text(perguntas[perguntaSelecionada]),
+             ElevatedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+             ),
+             ElevatedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+             ),
+             ElevatedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+             )
+          ],
+        )
+      ),
     );
   }
 
+}
+
+class PerguntaApp extends StatefulWidget{
+
+  PerguntaAppState createState() {
+    return PerguntaAppState();
+  }
+  
 }
