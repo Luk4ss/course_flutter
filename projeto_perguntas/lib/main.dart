105a887b2a8f5646bb5e4217fd0c3e1bacf87f43
= <lucasregoaraujo@gmail.com>
2023-02-26 19:51:56 -0300

    Componentes questao e reposta criados.

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'questao.dart';
+import 'resposta.dart';
 
 void main() => runApp(PerguntaApp());
   
@@ -28,19 +30,10 @@ class _PerguntaAppState extends State<PerguntaApp>{
         ),
         body: Column(
           children: [
-             Text(perguntas[_perguntaSelecionada]),
-             ElevatedButton(
-              child: Text('Resposta 1'),
-              onPressed: _responder,
-             ),
-             ElevatedButton(
-              child: Text('Resposta 2'),
-              onPressed: _responder,
-             ),
-             ElevatedButton(
-              child: Text('Resposta 3'),
-              onPressed: _responder,
-             )
+             Questao(perguntas[_perguntaSelecionada]),
+             Resposta('Resposta 1', _responder),
+             Resposta('Resposta 2', _responder),
+             Resposta('Resposta 3', _responder),
           ],
         )
       ),
