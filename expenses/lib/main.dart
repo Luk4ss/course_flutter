8626a06ecbdc61e77d0f58abbb4b077d8da7f480
= <lucasregoaraujo@gmail.com>
2023-04-03 15:55:59 -0300

    Estilização dos Container e implementação do toStringAsFixed

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,7 +1,5 @@
-
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
-
 
 void main() => runApp(const ExpensesApp());
 
@@ -10,9 +8,7 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: MyHomePage()
-    );
+    return MaterialApp(home: MyHomePage());
   }
 }
 
@@ -20,32 +16,74 @@ class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
   final _transactions = [
-    Transaction(id: 't1', title: 'Novo Tênis de Corrida', value: 310.76, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Conta de Luz', value: 221.30, date: DateTime.now())
+    Transaction(
+        id: 't1',
+        title: 'Novo Tênis de Corrida',
+        value: 310.76,
+        date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'Conta de Luz', value: 221.30, date: DateTime.now())
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Despesas Pessoais')
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children:  [
-          Container(
-            child: Card(
-              color: Colors.blue,
-              child: Text('Gráfico'),
-              elevation: 5,
+        appBar: AppBar(title: const Text('Despesas Pessoais')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 2)),
+              padding: const EdgeInsets.all(10),
+              child: const Card(
+                color: Colors.blue,
+                elevation: 5,
+                child: Text('Gráfico'),
+              ),
             ),
-          ),
-          Card(
-            child: Text('Lista De Transações'),
-          )
-        ],
-      )
-    );
+            Column(
+              children: _transactions.map((tr) {
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                                    horizontal: 15, 
+                                    vertical: 10),
+                      decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.purple, width: 2)
+                                  ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text('R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.purple)),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
+                          style: const TextStyle(
+                              fontSize: 16, 
+                              fontWeight: FontWeight.bold
+                        ),
+                        ),
+                        Text(tr.date.toString(),
+                            style: const TextStyle(
+                              color: Colors.grey)
+                        )
+                      ],
+                    )
+                  ],
+                ));
+              }).toList(),
+            )
+          ],
+        ));
   }
 }
