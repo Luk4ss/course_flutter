a643ee4d2200e020cc3316bff9a2ef898ff92a79
= <lucasregoaraujo@gmail.com>
2023-04-19 19:31:23 -0300

    Adicionando uma imagem no projeto, caso não haja itens na lista de transaçãoes.

--- expenses/lib/components/transaction_list.dart
+++ expenses/lib/components/transaction_list.dart
@@ -14,7 +14,13 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
+      child: transactions.isEmpty ? Column(
+        children: [
+          Text('Nenhuma transação cadastrada!'),
+          SizedBox(height: 20),
+          Container(height: 200, child: Image.asset('assets\\images\\waiting.png', fit: BoxFit.cover,))
+        ],
+      ) : ListView.builder(
         //Quantidade de items do ListView
         itemCount: transactions.length,
 
